9b9420d8e8d6c29c371f39473b77d82d00d08a62
fawzy-ibrhim <fawzy_ibrhim@hotmail.com>
2020-05-08 02:25:53 +0300

    add more files

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,21 +1,27 @@
-include stdlib
 #Manage Chrony
 class pbg_chrony {
   
   ensure_packages(['chrony'])
+  
+  file { '/etc/chrony.conf':
+    ensure  => present,
+    source  => 'puppet:///modules/pbg_chrony/chrony.conf',
+    notify  => Service['notify'],
+    require => Package['chrony'],
+  }
 
-  if $facts['os']['family'] == 'RedHat' {
+  if $facts['os']['familyi'] == 'RedHat' {
     service { 'chrony':
       ensure => running,
       enable => true,
       name   => 'chronyd',
-    }   
+    }
   } else {
     service { 'chrony':
       ensure => running,
       enable => true,
       name   => 'chrony',
-    }   
+    }
   }
 
 }
